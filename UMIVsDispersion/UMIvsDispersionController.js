7f496d97a64e5ef44d56ed02107d4a8339d314d2
Justin Lee <justinlee0022@gmail.com>
2018-01-11 09:52:33 -0800

    Basic graph is working

--- UMIVsDispersion/UMIvsDispersionController.js
+++ UMIVsDispersion/UMIvsDispersionController.js
@@ -1,5 +1,5 @@
 /* global $ */
-import { loadUMIVsDispersion, loadOptions } from '../../../service/overviewDataService'
+import { options, data } from '../data'
 import drawUMIVsDispersion from './drawUMIVsDispersion'
 
 const attachGraph = (data, umi = 'UMI6') => {
@@ -27,37 +27,28 @@ const initUMIVsDispersion = () => {
   const UMI = ['UMI1', 'UMI2', 'UMI3', 'UMI4', 'UMI5', 'UMI6']
   const elements = [deptSelect, yearSelect, termSelect, umiSelect, belowMinSelect]
 
-  loadOptions()
-    .then(options => options[0])
-    .then(options => {
-      options.depts.unshift('all')
-      options.terms.unshift('all')
-      return options
-    })
-    .then(options => {
-      deptSelect.value = options.depts[0]
-      umiSelect.value = 'UMI6'
+  options.depts.unshift('all')
+  options.terms.unshift('all')
 
-      deptSelect.innerHTML = attachOptions(options.depts)
-      yearSelect.innerHTML = attachOptions(options.years)
-      termSelect.innerHTML = attachOptions(options.terms)
-      umiSelect.innerHTML = attachOptions(UMI)
+  deptSelect.value = options.depts[0]
+  umiSelect.value = 'UMI6'
 
-      $('#umiVsDispersionDept').selectpicker('val', options.depts[0])
-      $('#umiVsDispersionYear').selectpicker('val', options.years[options.years.length - 1])
-      $('#umiVsDispersionTerm').selectpicker('val', options.terms[0])
-      $('#umiVsDispersionUMI').selectpicker('val', 'UMI6')
+  deptSelect.innerHTML = attachOptions(options.depts)
+  yearSelect.innerHTML = attachOptions(options.years)
+  termSelect.innerHTML = attachOptions(options.terms)
+  umiSelect.innerHTML = attachOptions(UMI)
 
-      refreshPicker()
-    })
-    .then(() => {
-      loadUMIVsDispersion({ dept: deptSelect.value, year: Number(yearSelect.value), term: termSelect.value, meetsMin: belowMinSelect.value })
-        .then(data => attachGraph(data))
-    })
+  $('#umiVsDispersionDept').selectpicker('val', options.depts[0])
+  $('#umiVsDispersionYear').selectpicker('val', options.years[options.years.length - 1])
+  $('#umiVsDispersionTerm').selectpicker('val', options.terms[0])
+  $('#umiVsDispersionUMI').selectpicker('val', 'UMI6')
+
+  refreshPicker()
+
+  attachGraph(data)
 
   elements.map(el => el.addEventListener('change', function () {
-    loadUMIVsDispersion({ dept: deptSelect.value, year: Number(yearSelect.value), term: termSelect.value, meetsMin: belowMinSelect.value })
-      .then(data => attachGraph(data, umiSelect.value))
+    attachGraph(data, umiSelect.value)
   }))
 }
 
