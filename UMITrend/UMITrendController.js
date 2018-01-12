ad8b7ebbc0d05160ee649aafef096d4f7d489b37
Justin Lee <justinlee0022@gmail.com>
2018-01-12 15:57:24 -0800

    Implementing ability to filter by instructor

--- UMITrend/UMITrendController.js
+++ UMITrend/UMITrendController.js
@@ -3,7 +3,7 @@ import { options, data } from '../data'
 const R = require('ramda')
 import Chart from 'chart.js'
 
-const attachOptions = arr => arr.map(name => '<option value="' + name + '">' + name + '</option>').join(' ')
+const attachOptions = arr => arr.map(x => '<option value="' + x + '">' + x + '</option>').join(' ')
 
 const instructorNames = data => R.uniq(data
   .map(sections => sections.instructorName)
@@ -11,11 +11,34 @@ const instructorNames = data => R.uniq(data
 )
 
 const UMITrendController = () => {
+  const UMI = ['UMI1', 'UMI2', 'UMI3', 'UMI4', 'UMI5', 'UMI6']
+
   const instructorSelect = document.getElementById('umiTrendInstructor')
   instructorSelect.innerHTML = attachOptions(instructorNames(data))
 
+  const umiSelect = document.getElementById('umiTrendUMI')
+  umiSelect.innerHTML = attachOptions(UMI)
+  umiSelect.value = 'UMI6'
+
   $('#umiTrendInstructor').selectpicker('val', instructorNames(data)[0])
   $('#umiTrendInstructor').selectpicker('refresh')
+  $('#umiTrendUMI').selectpicker('refresh')
+
+  drawChart(instructorSelect.value)
+
+  instructorSelect.addEventListener('change', function () {
+    drawChart(instructorSelect.value)
+  })
+}
+
+const drawChart = instructorName => {
+  const instructorData = data.filter(section => section.instructorName === instructorName)
+
+  const ctx = document.getElementById('umiTrendInstructor').getContext('2d')
+
+  return new Chart(ctx, {
+
+  })
 }
 
 export default UMITrendController
--- UMIVsDispersion/UMIvsDispersionController.js
+++ UMIVsDispersion/UMIvsDispersionController.js
@@ -1,6 +1,7 @@
 /* global $ */
 import { options, data } from '../data'
 import drawUMIVsDispersion from './drawUMIVsDispersion'
+const R = require('ramda')
 
 const attachGraph = (data, umi = 'UMI6') => {
   const svg = document.getElementById('UMIVsDispersionSVG')
@@ -12,12 +13,14 @@ const attachGraph = (data, umi = 'UMI6') => {
 const attachOptions = arr => arr.map(x => '<option value="' + x + '">' + x + '</option>').join(' ')
 
 const refreshPicker = () => {
+  $('#umiVsDispersionInstructor').selectpicker('refresh')
   $('#umiVsDispersionYear').selectpicker('refresh')
   $('#umiVsDispersionTerm').selectpicker('refresh')
   $('#umiVsDispersionUMI').selectpicker('refresh')
 }
 
 const initUMIVsDispersion = () => {
+  const instructorSelect = document.getElementById('umiVsDispersionInstructor')
   const yearSelect = document.getElementById('umiVsDispersionYear')
   const termSelect = document.getElementById('umiVsDispersionTerm')
   const umiSelect = document.getElementById('umiVsDispersionUMI')
@@ -31,10 +34,18 @@ const initUMIVsDispersion = () => {
 
   umiSelect.value = 'UMI6'
 
+  console.log(R.uniq(data.map(sections => sections.instructorName)).sort())
+
+  const instructorNames = R.uniq(data.map(sections => sections.instructorName)).sort()
+
+  instructorNames.unshift('all')
+
+  instructorSelect.innerHTML = attachOptions(instructorNames)
   yearSelect.innerHTML = attachOptions(options.years)
   termSelect.innerHTML = attachOptions(options.terms)
   umiSelect.innerHTML = attachOptions(UMI)
 
+  $('#umiVsDispersionInstructor').selectpicker('val', instructorNames[0])
   $('#umiVsDispersionYear').selectpicker('val', options.years[options.years.length - 1])
   $('#umiVsDispersionTerm').selectpicker('val', options.terms[0])
   $('#umiVsDispersionUMI').selectpicker('val', 'UMI6')
@@ -42,7 +53,7 @@ const initUMIVsDispersion = () => {
   refreshPicker()
 
   const filterData = data => data
-    .filter(x => x.dept === 'FNH')
+    .filter(x => instructorSelect.value === 'all' ? true : x.instructorName === instructorSelect.value)
     .filter(x => yearSelect.value === 'all' ? true : x.year === Number(yearSelect.value))
     .filter(x => termSelect.value === 'all' ? true : termSelect.value === x.term)
     .filter(x => belowMinSelect.value === 'true' ? x.meetsMin : true)
